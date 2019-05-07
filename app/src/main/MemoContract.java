b46327a1f85de7fc88911cb3cbda6058efec4313
kimwongiCS <50092014+kimwongiCS@users.noreply.github.com>
2019-05-07 20:01:02 +0900

    Add files via upload

---
+++ app/src/main/MemoContract.java
@@ -0,0 +1,15 @@
+package com.example.tryy;
+
+import android.provider.BaseColumns;
+
+public final class MemoContract {
+    private  MemoContract(){
+
+    }
+
+    public static class MemoEntry implements BaseColumns {
+        public static final String TABLE_NAME = "memo";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_CONTENTS = "contents";
+    }
+}
