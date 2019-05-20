1f87103b88844a4d9403389065b005e844b64009
Digistor <wowns0903@gmail.com>
2019-05-20 09:52:20 +0900

    글쓰기 구현 (여상혁)

--- page/board/write_ok.php
+++ page/board/write_ok.php
@@ -1,29 +1,25 @@
 <?php
- $connect = mysqli_connect("tryy.iptime.org", "root", "root", "tryy") or die("fail");
-include "../../db.php";
-$date = date('Y-m-d');
-$id = $_GET[name];                      //Writer
-$pw = $_GET[pw];                        //Password
-$title = $_GET[title];                  //Title
-$content = $_GET[content];
-                $URL = '../../board.php';                   //return URL
- 
- 
-                $query = "insert into post_board (idx,name,pw, title,content,date,hit) 
-                        values(null,'$id',$pw', '$title', '$content',$date, 0)";
- 
- 
-                $result = $connect->query($query);
-                if($result){
-?>                  <script>
-                        alert("<?php echo "글이 등록되었습니다."?>");
-                        location.replace("<?php echo $URL?>");
-                    </script>
+	include "../../db.php";
+
+	$date = date('Y-m-d');
+	$id = $_POST['name'];                      //Writer
+	$pw = $_POST['pw'];                        //Password
+	$title = $_POST['title'];                  //Title
+	$content = $_POST['content'];
+	$URL = '../../board.php';                   //return URL
+
+	$query = "insert into post_board(name, pw, title, content, date) values('$id', '$pw', '$title', '$content', '$date')";
+
+	$result = mq($query);
+	if(isset($result)){
+?>                  
+	<script>
+		alert("<?php echo "글이 등록되었습니다."?>");
+		location.replace("<?php echo $URL?>");
+	</script>
 <?php
-                }
-                else{
-                        echo "FAIL";
-                }
- 
-                mysqli_close($connect);
+	}
+	else{
+		echo "FAIL";
+	}
 ?>
